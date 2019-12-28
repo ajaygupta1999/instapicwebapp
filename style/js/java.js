8056351998af707ee3472c44a49b522a88b3532f
ajay <ajay.u.gupta14@gmail.com>
2019-12-28 20:53:23 +0000

    no change

--- style/js/java.js
+++ style/js/java.js
@@ -13,8 +13,7 @@ function imgcal(){
              var lastdata = imgurl.substr(60 , imgurl.length);
              var c_w = img.clientWidth;
              var c_h = img.clientHeight;
-             var pixelRatio = window.devicePixelRatio || 1.0;
-             var parameters = "c_fill,w_" + c_w*pixelRatio + ",h_" +  c_h*pixelRatio +",g_auto,f_auto";
+             var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
              var url = dataurl + parameters + "/" + lastdata;
              img.setAttribute("src" , url);
 		     img.style.setProperty("transform" , "rotate(0deg)");
@@ -32,8 +31,7 @@ function imgcal(){
              var lastdata = imgurl.substr(60 , imgurl.length);
              var c_h = image.clientWidth;
              var c_w = image.clientHeight;
-             var pixelRatio = window.devicePixelRatio || 1.0;
-             var parameters = "c_fill,w_" + c_w*pixelRatio + ",h_" +  c_h*pixelRatio +",g_auto,f_auto";
+             var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
              var url = dataurl + parameters + "/" + lastdata;
              image.setAttribute("src" , url); 
            });
