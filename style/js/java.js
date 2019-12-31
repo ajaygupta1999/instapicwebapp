fbf1051028856e850714376e2ea5f53f2ee7a428
ajay <ajay.u.gupta14@gmail.com>
2019-12-31 07:27:31 +0000

    image processing changed

--- style/js/java.js
+++ style/js/java.js
@@ -10,7 +10,7 @@ function imgcal(){
 	  if(showFirstImg == 90 || showFirstImg == 270){
 	   	     orientation.setAttribute("class" , "potrate");
              var imgurl = img.src;
-             var lastdata = imgurl.substr(60 , imgurl.length);
+             var lastdata = imgurl.substr(52 , imgurl.length);
              var c_w = img.clientWidth;
              var c_h = img.clientHeight;
              var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
@@ -19,23 +19,36 @@ function imgcal(){
 		     img.style.setProperty("transform" , "rotate(0deg)");
 	  }else if(img.naturalWidth > img.naturalHeight){
 		  orientation.setAttribute("class" , "landscape");
-		  imageset(img);
+		  singleimageset(img);
+	  }else if (img.naturalWidth < img.naturalHeight){
+		  orientation.setAttribute("class" , "potrate");
+		  singleimageset(img);
 	  }else{
 		  orientation.setAttribute("class" , "square");
-		  imageset(img);
+		  singleimageset(img);
 	  }
 	
 	  function imageset(images){
 		 images.forEach(function(image){
              var imgurl = image.src;
              var lastdata = imgurl.substr(60 , imgurl.length);
-             var c_h = image.clientWidth;
-             var c_w = image.clientHeight;
+             var c_w = image.clientWidth;
+             var c_h = image.clientHeight;
              var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
              var url = dataurl + parameters + "/" + lastdata;
              image.setAttribute("src" , url); 
            });
 	   }
+	
+	function singleimageset(image){
+             var imgurl = image.src;
+             var lastdata = imgurl.substr(60 , imgurl.length);
+             var c_w = image.clientWidth;
+             var c_h = image.clientHeight;
+             var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
+             var url = dataurl + parameters + "/" + lastdata;
+             image.setAttribute("src" , url); 
+	}
 	
   }
 
