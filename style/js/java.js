c4c9e15309e3802790a206be95268c3a070e36a4
ajay <ajay.u.gupta14@gmail.com>
2020-01-03 06:04:21 +0000

    image processing improvenment

--- style/js/java.js
+++ style/js/java.js
@@ -1,6 +1,6 @@
 function imgcal(){
 	  var dataurl =  "https://res.cloudinary.com/instapic-heroku-app/image/upload/";
-      var images = document.querySelectorAll(".AllSmallImages");
+      var images = document.querySelectorAll("[data-src]");
       imageset(images);
 	
 	  var orientation = document.querySelector("#img-box");
@@ -8,14 +8,15 @@ function imgcal(){
 	  orientation.setAttribute("class" , "none");
 	  var  showFirstImg = document.querySelector("#checkimg").getAttribute("alt");
 	  if(showFirstImg == 90 || showFirstImg == 270){
-	   	     orientation.setAttribute("class" , "potrate");
-             var imgurl = img.src;
-             var lastdata = imgurl.substr(60 , imgurl.length);
-             var c_w = img.clientWidth;
-             var c_h = img.clientHeight;
-             var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
-             var url = dataurl + parameters + "/" + lastdata;
-             img.setAttribute("src" , url);
+		  console.log("hii there");
+		     orientation.setAttribute("class" , "potrate");
+		     var imgurl = img.src;
+		     var lastdata = imgurl.substr(60 , imgurl.length);
+		     var c_w = img.clientWidth;
+		     var c_h = img.clientHeight;
+		     var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
+		     var url = dataurl + parameters + "/" + lastdata;
+		     img.setAttribute("src" , url);
 		     img.style.setProperty("transform" , "rotate(0deg)");
 	  }else if(img.naturalWidth > img.naturalHeight){
 		  orientation.setAttribute("class" , "landscape");
@@ -30,12 +31,13 @@ function imgcal(){
 	
 	  function imageset(images){
 		 images.forEach(function(image){
-             var imgurl = image.src;
+             var imgurl = image.getAttribute("data-src");
              var lastdata = imgurl.substr(60 , imgurl.length);
              var c_w = image.clientWidth;
              var c_h = image.clientHeight;
              var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
              var url = dataurl + parameters + "/" + lastdata;
+			 image.removeAttribute("data-src");
              image.setAttribute("src" , url); 
            });
 	   }
