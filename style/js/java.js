ca480aa3b44c3909877a0142ce4b19f3680a6366
ajay <ajay.u.gupta14@gmail.com>
2020-03-02 19:11:00 +0000

    improved image loading performance

--- style/js/java.js
+++ style/js/java.js
@@ -1,31 +1,38 @@
-function imgcal(){
+async function imgcal(){
 	  var dataurl =  "https://res.cloudinary.com/instapic-heroku-app/image/upload/";
       var images = document.querySelectorAll("[data-src]");
       imageset(images);
-	
+	  
+	  try{
+		  var firstShowImg = document.querySelector("#firstShowImg");
+		  let image_width = await firstShowImg.naturalWidth;
+	      let image_height = await firstShowImg.naturalHeight;
+		  
 	  var orientation = document.querySelector("#img-box");
 	  var img = document.querySelector("#checkimg");
-	  var firstShowImg = document.querySelector("#firstShowImg");
 	  var  showFirstImg = document.querySelector("#checkimg").getAttribute("alt");
 	  if(showFirstImg == 90 || showFirstImg == 270){
 		     orientation.setAttribute("class" , "potrate");
 		     var imgurl = firstShowImg.src;
-		     var lastdata = imgurl.substr(60 , imgurl.length);
+		     var lastdata = imgurl.substr( 60, imgurl.length);
 		     var c_w = img.clientWidth;
 		     var c_h = img.clientHeight;
 		     var parameters = "c_fill,w_" + c_w + ",h_" +  c_h +",g_auto,f_auto";
 		     var url = dataurl + parameters + "/" + lastdata;
 		     img.setAttribute("src" , url);
 		     img.style.setProperty("transform" , "rotate(0deg)");
-	  }else if(firstShowImg.naturalWidth > firstShowImg.naturalHeight){
+	  }else if(image_width > image_height){
 		  orientation.setAttribute("class" , "landscape");
 		  singleimageset(img);
-	  }else if (firstShowImg.naturalWidth < firstShowImg.naturalHeight){
+	  }else if (image_width < image_height){
 		  orientation.setAttribute("class" , "potrate");
 		  singleimageset(img);
 	  }else{
 		  orientation.setAttribute("class" , "square");
 		  singleimageset(img);
+	  }
+	  } catch(err){
+		  console.log(err);
 	  }
 	
 	  function imageset(images){
@@ -56,5 +63,4 @@ function imgcal(){
   }
 
 
- 
 imgcal(); 
